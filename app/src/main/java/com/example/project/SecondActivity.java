07d1ea1c8a14fa935087bb7afd1908bad3c10d0c
William Lindholm <a22willi@student.his.se>
2023-05-02 18:24:49 +0200

    Added buttonlistener that saves the data currently in the editText to shared preference.

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,8 +2,12 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -15,9 +19,23 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        SharedPreference preference = new SharedPreference();
+
+        myPreferenceRef = preference.getMyPreferenceRef();
         myPreferenceRef = getPreferences(MODE_PRIVATE);
         myPreferenceEditor = myPreferenceRef.edit();
 
+        final TextView editText = findViewById(R.id.editText);
+        Button saveButton = findViewById(R.id.saveButton);
+        editText.setText(myPreferenceRef.getString("MyPersistentData", ""));
 
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myPreferenceEditor.putString("MyPersistentData", editText.getText().toString());
+                myPreferenceEditor.apply();
+                finish();
+            }
+        });
     }
 }
