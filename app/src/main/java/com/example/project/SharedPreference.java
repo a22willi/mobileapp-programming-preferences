7015cb07e77b5d1a1dc441afa67dcf7c3c728823
William Lindholm <a22willi@student.his.se>
2023-05-02 18:25:16 +0200

    Created separate class for shared preference.

---
+++ app/src/main/java/com/example/project/SharedPreference.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
+
+public class SharedPreference {
+
+
+    private SharedPreferences myPreferenceRef;
+
+    public SharedPreferences getMyPreferenceRef() {
+        return myPreferenceRef;
+    }
+}
