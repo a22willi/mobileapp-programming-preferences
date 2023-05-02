d41764e6abb2f961bfb4a89b1d607ad56f7bd096
William Lindholm <a22willi@student.his.se>
2023-05-02 11:28:02 +0200

    Added OnClickListener to Button that opens SecondActivity.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +13,6 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences myPreferenceRef;
-    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +22,17 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         myPreferenceRef = getPreferences(MODE_PRIVATE);
-        myPreferenceEditor = myPreferenceRef.edit();
 
         TextView preferenceView = findViewById(R.id.preferenceText);
         preferenceView.setText(myPreferenceRef.getString("MypersistentData", "No preference found."));
-    }
 
+        Button editData = findViewById(R.id.editData);
+        editData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
 }
